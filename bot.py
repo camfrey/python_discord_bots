fba129f0e950ea621d736a727cb2104066dac510
Cameron Frey <camfrey@pop-os.localdomain>
2020-11-15 22:27:29 -0500

    Initialization of repository

---
+++ bot.py
@@ -0,0 +1,36 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    
+    if message.author == client.user:
+        return
+    msg = message.content.split(" ")
+    
+    for word in msg:
+        resp = "first of all..... "
+        wordlen = len(word)
+        print(wordlen)
+        print(word)
+        if(wordlen<=2):
+            continue
+        if (word[wordlen-1]=='a') or (word[wordlen-1]=='A'):
+            resp = resp + word[:wordlen-1]+"er? I don't even know her!"
+        elif (word[wordlen-1]=='r' or word[wordlen-1]=='R') and (word[wordlen-2]=='e' or word[wordlen-2]=='E'):
+            resp = resp + word[:wordlen-2] +" her? I don't even know her!"
+        # print(resp)
+        if((word[wordlen-1]=='a') or (word[wordlen-1]=='A')) or ((word[wordlen-1]=='r' or word[wordlen-1]=='R') and (word[wordlen-2]=='e' or word[wordlen-2]=='E')):
+            await message.channel.send(resp)
+
+        # await message.channel.send(word)
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('Nzc3NzIwNDg4OTM1Njg2MTU1.X7HipQ.vSsdnFF9m4el_91lTS_XL62C8qg')
